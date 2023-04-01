91b76c0a971b4e6b7c16eb72649e78073e069140
JasperOtieno <jaotie@gmail.com>
2023-04-01 20:30:55 +0300

    First update

---
+++ Packages.R
@@ -0,0 +1,16 @@
+#Packages for Rtraining
+
+
+library(tidyverse) #for data cleaning, manipulation, transformation, visualization
+
+library(haven) #for improting various types of data formats
+library(magrittr) #for piping i.e %>% 
+
+install.packages("gmodels") #
+library(gmodels)
+
+install.packages("table1")
+library(table1)
+
+library(readxl)
+
