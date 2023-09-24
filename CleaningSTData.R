e9e8ff098949a012d2fb6e967c70d24f0c9a2eb6
JasperOtieno <jaotie@gmail.com>
2023-09-25 00:46:21 +0300

    Updated

--- CleaningSTData.R
+++ CleaningSTData.R
@@ -42,7 +42,8 @@ STIData <- STIData %>%
                                 STIData$Sex...47=="Female" & is.na(STIData$Sex...35) ~"Female",
                                 is.na(STIData$Sex...47) & STIData$Sex...35=="Male" ~"Male",
                                 TRUE ~"No sex" #reassign values
-                             ))
+                             )) %>% 
+  relocate(Sex, .after=A1Age)
 
 (missex<-STIData[is.na(STIData$Sexdif),c(1,35,47:49)]) #output those missing sex entry
 
@@ -50,7 +51,7 @@ STIData<- STIData %>%
   select(-c(Sex...35,Sex...47,Sexdif)) #remove the sex vars after creating clean one
 
 
-# Check for duplicates on subject identifier and rows and clean
+#Check for duplicates on subject identifier and rows and clean
 
 dups <- janitor::get_dupes(STIData) %>% 
   select(IdNumber,dupe_count, Date, A1Age, A2Occupation, Weight, Height) # save duplicate rows
@@ -58,7 +59,6 @@ dups <- janitor::get_dupes(STIData) %>%
 (dupID<-janitor::get_dupes(STIData,IdNumber) %>% 
   select(IdNumber,dupe_count, Date, A1Age, A2Occupation, Weight, Height)) #save rows with duplicate IDs as another dataset
 
-#dups <- STIData[duplicated(STIData$IdNumber), ] analogous to line 58-59
 #sum(duplicated(STIData$IdNumber)) #get total number of duplicates
 #sort(unique(STIData$IdNumber))
 
@@ -66,13 +66,13 @@ dups <- janitor::get_dupes(STIData) %>%
 
 STIData <- STIData %>%
   dplyr::mutate(IdNumber2= case_when(STIData$IdNumber==51 & STIData$A1Age == 23 ~ 227,
-                            TRUE~STIData$IdNumber))   
-STIData <- STIData %>% 
-  select(-c(IdNumber)) %>% 
-  rename(IdNumber=IdNumber2) %>% 
-  relocate(IdNumber, .before = CaseStatus)
+                            TRUE~STIData$IdNumber)) %>% 
+        select(-c(IdNumber)) %>% 
+        rename(IdNumber=IdNumber2) %>% 
+        relocate(IdNumber, .before = CaseStatus)
 
 #Check type of the Id number and convert to character if needed
+
 class(STIData$IdNumber)
 STIData$IdNumber <- as.character(STIData$IdNumber)
 
@@ -89,86 +89,69 @@ table(STIData$CaseStatus)
 STIData <- STIData %>%
   dplyr::mutate(CaseStatus2= case_when(STIData$CaseStatus==3 & STIData$IdNumber == 31 ~ 1,
                                        STIData$CaseStatus==3 & STIData$IdNumber == 1 ~ 2,
-                                       TRUE~STIData$CaseStatus)) #reassign values
+                                       TRUE~STIData$CaseStatus)) %>% #reassing values
+          select(-c(CaseStatus)) %>% 
+          rename(CaseStatus=CaseStatus2) %>% 
+          relocate(CaseStatus, .after = IdNumber)
+  
 
-STIData <- STIData %>% 
-  select(-c(CaseStatus)) %>% 
-  rename(CaseStatus=CaseStatus2) %>% 
-  relocate(CaseStatus, .after = IdNumber)
+#Convert CaseStatus to a factor and label
+class(STIData$CaseStatus)
+
+STIData$CaseStatus <- factor(STIData$CaseStatus,
+                               levels = 1:2,
+                               labels = c("Positive", "Negative"))  #assign labels
 
 #confirm cleaned status
 table(STIData$CaseStatus)
 
+#Clean Date
+table(STIData$Date) #nothing to clean
+
+#Clean A1Age
+table(STIData$A1Age)
+
+STIData<- STIData %>% 
+  mutate(AgeCat= case_when(A1Age <18 ~ "Below 18 Years",
+                           A1Age >=18 & A1Age <=35 ~ "18 to 35 Years",
+                           A1Age>35 & A1Age<=50 ~ "36 to 50 Years",
+                           A1Age>50 ~ "Above 50 Years",
+                           TRUE~ "Not categorized")) %>% 
+  relocate(AgeCat, .after=A1Age)
+
+#Confirm age categories
+table(STIData$AgeCat)
+
+
+# Removing numbers from string values
+
+STIData<- STIData %>% 
+  dplyr::mutate(A2Occupation = tolower(str_replace(A2Occupation, "\\d", "")),
+                A3Church = str_replace(A3Church, "\\d", ""),
+                A4LevelOfEducation = str_replace(A4LevelOfEducation, "\\d", ""),
+                A5MaritalStatus = str_replace(A5MaritalStatus, "\\d", ""),
+                D2Group1 = str_replace(D2Group1, "\\d", ""),
+                D2Group2 = str_replace(D2Group2, "\\d", ""),
+                E8WhyhaveSTI = str_replace(E8WhyhaveSTI, "\\d", ""),
+                N10givereceiveforsex = str_replace(N10givereceiveforsex, "\\d", ""),
+                N11Usedcondom = str_replace(N11Usedcondom, "\\d", ""),
+                N12UseCondom = str_replace(N12UseCondom, "\\d", ""),
+                N13TakenAlcohol = str_replace(N13TakenAlcohol, "\\d", ""),
+                Typeofsti = str_replace(Typeofsti, "\\d", ""),
+                N9Relationship = str_replace(N9Relationship, "\\d", ""))
+
 
 ###########################################################################################
-# save records duplicated by ID Number
-dups2 <- STIData[STIData$IdNumber == 51, ]
-
-# Reassign ID number for subject 51, when age ==  23
-
-STIData$IdNumber[STIData$IdNumber == 51 & STIData$A1Age == 23] <- 227
-
-# check duplicates again
-sum(duplicated(STIData$IdNumber))
-
-# check Case status
-table(STIData$CaseStatus)
-
-case3 <- STIData[STIData$CaseStatus == 3, ]
-
-STIData$CaseStatus[STIData$CaseStatus == 3 & STIData$IdNumber == 31] <- 1
-STIData$CaseStatus[STIData$CaseStatus == 3 & STIData$IdNumber == 1] <- 2
-
-# Reassign proper codes to CaseStatus
-STIData1 <- 
-  STIData %>% 
-  dplyr::mutate(CaseStatus1 = dplyr::if_else(CaseStatus == 2, 0, CaseStatus)) %>% 
-  dplyr::relocate(c(CaseStatus1, Sex...47), .after = CaseStatus)
-
-
-STIData1$CaseStatus1 <- factor(STIData1$CaseStatus1,
-                               levels = 0:1,
-                               labels = c("Negative", "Positive"))
-
-
-class(STIData1$CaseStatus1)
-
-# Drop unnecessary variables 
-STIData1 <- 
-  STIData1 %>% 
-  dplyr::select(-c(CaseStatus, C3StiYesno))
-
 
 # Rename variables 
-STIData2 <- 
-  STIData1 %>% 
-  dplyr::rename(CaseStatus = CaseStatus1,
-                ID = IdNumber,
-                Sex = Sex...47,
-                Age = A1Age,
-                Occupation = A2Occupation,
-                Church = A3Church,
-                `Level Of Education` = A4LevelOfEducation)
+# STIData2 <- 
+#   STIData1 %>% 
+#   dplyr::rename(CaseStatus = CaseStatus1,
+#                 ID = IdNumber,
+#                 Sex = Sex...47,
+#                 Age = A1Age,
+#                 Occupation = A2Occupation,
+#                 Church = A3Church,
+#                 `Level Of Education` = A4LevelOfEducation)
 
-# Grouping numerical variable
-STIData2 <- 
-  STIData2 %>% 
-  dplyr::mutate(
-    AgeCat = case_when(
-      Age < 18 ~ "Below 18",
-      Age >= 18 & Age < 30 ~ "18 - 30",
-      Age >= 30 & Age < 50 ~ "30 - 50",
-      Age >= 50 ~ "Above 50"
-    )
-  ) %>% 
-  dplyr::relocate(AgeCat, .after = Age)
 
-table(STIData2$AgeCat, STIData2$Sex)
-
-# Removing numbers in string values
-STIData2 <- 
-  STIData2 %>% 
-  dplyr::mutate(Occupation = toupper(stringr::str_replace(Occupation, "\\d", "")),
-                Church = stringr::str_replace(Church, "\\d", ""),
-                `Level Of Education` = stringr::str_replace(`Level Of Education`, "\\d", ""),
-                A5MaritalStatus = stringr::str_replace(A5MaritalStatus, "\\d", ""),)
